2bad8999959159a6a343e25e5476e2985f34b076
nthinh226 <ngocthinh1126@gmail.com>
2022-02-25 16:04:04 +0700

    Initial commit

---
+++ app/src/main/java/com/nt/geoquiz/MainActivity.java
@@ -0,0 +1,38 @@
+package com.nt.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, R.string.true_button, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,100,100);
+                toast.show();
+            }
+        });
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, R.string.false_button, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,100,250);
+                toast.show();
+            }
+        });
+    }
+}
